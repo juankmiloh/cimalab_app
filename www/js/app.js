62ab6ebf5ff023b83e61cbbbdf2b33e475dabbab
Juan Camilo Herrera <juankmiloardila@gmail.com>
2017-08-16 16:29:35 -0500

    Creando archivos para generar la aplicación móvil con ionic

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,11 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+})
